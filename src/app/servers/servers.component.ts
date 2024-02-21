dc7ecd1afc30396b28ef444918b3dc76e84a64d2
Tan Nguyen Hoang Nhat <tan.nguyenhoangnhat@bravosuite.io>
2024-02-21 16:48:06 +0700

    Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverName = 'Test Server Name';
+  serverCreationStatus = 'Server status';
   
   constructor(){
     setTimeout(() => {
@@ -16,6 +18,15 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
+  }
+
+  onCreateServer()
+  {
+    this.serverCreationStatus = "Server was created! Name is: " + this.serverName;
+  }
+
+  onUpdateServerName(event: any)
+  {
+    this.serverName = event.target.value;
   }
 }
