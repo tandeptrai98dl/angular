0bc38123b0a90c5e75bc2cf805804a1dec87114d
Tan Nguyen Hoang Nhat <tan.nguyenhoangnhat@bravosuite.io>
2024-02-22 18:31:51 +0700

    Done Section2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverName = 'Test Server Name';
   serverCreationStatus = 'Server status';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
   
   constructor(){
     setTimeout(() => {
@@ -22,6 +24,8 @@ export class ServersComponent implements OnInit {
 
   onCreateServer()
   {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created! Name is: " + this.serverName;
   }
 
